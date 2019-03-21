9db54c31add3417cf74c93f15d8babf8f6938585
Pawel Kaput <pawel.kaput@outlook.com>
2019-03-21 12:47:48 +0100

    Routes defined for top commenters rank

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "home#welcome"
+  get  "/top_commenters", to: "home#ranking"
   resources :genres, only: :index do
     member do
       get "movies"
